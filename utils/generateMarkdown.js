6513adc7d45ba189a1500a32de5fcee0159a7043
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-09-27 18:46:04 -0600

    finally working

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,26 @@
-// function to generate markdown for README
+const { clear } = require('console');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+
+const writeToFile = fileContent => {
+  console.log(writeToFile)
+  return new Promise((resolve, reject) => {
+    fs.writeToFile('./README.md', fileContent, err => {
+      if(err) {
+        reject(err);
+        return;
+      }
+      resolve({
+        ok: true,
+        message: 'File Created'
+      })
+    })
+  })
+  
+}
+
+// // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   
@@ -6,5 +28,9 @@ function generateMarkdown(data) {
 `;
 
 }
+writeToFile('./README.md')
+.then(data => console.log(data))
+.catch(err => console.log(err))
+
 
 module.exports = generateMarkdown;
